21c88d82b14a0d95f076051bea321c139f6b238a
Juan Morales <50474035-jrm31@users.noreply.replit.com>
2026-08-02 18:59:48 +0000

    Fix eas update: add babel transforms for Hermes private class fields

--- artifacts/patient-images-mobile/babel.config.js
+++ artifacts/patient-images-mobile/babel.config.js
@@ -2,5 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    plugins: [
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+    ],
   };
 };
